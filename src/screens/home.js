24b22d119e81a2de696da7657ee5381266e7854c
mostafizur99 <coder.mr11@gmail.com>
2023-02-18 19:31:40 +0600

    feat: item seatch-filter added

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,5 +1,5 @@
 import { FlatList, SafeAreaView, StyleSheet, TextInput, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import AppHeader from '../components/header/app-header'
 import { colors } from '../theme/colors'
 import { MOUNTAIN_LIST } from '../data/mountain-list'
@@ -7,23 +7,30 @@ import { spacing } from '../theme/spacing'
 import MountainItem from '../components/mountains/mountain-item'
 
 export default function Home({ navigation }) {
+    const [list, setList] = useState(MOUNTAIN_LIST)
+
+    const searchFilter = (text) => {
+        const filteredIList = MOUNTAIN_LIST.filter((item) => {
+            return (item.name.toLowerCase()).indexOf(text.toLowerCase()) > -1
+        })
+        setList(filteredIList)
+    }
+
     return (
         <SafeAreaView style={styles.container}>
             <AppHeader />
 
             <TextInput
-                placeholder='Type Mountain Name'
+                placeholder='Search Mountain Name'
                 placeholderTextColor={colors.white}
                 autoCorrect={false}
                 style={styles.searchInput}
-                onChangeText={(text) => {
-                    console.log('Text', text)
-                }}
+                onChangeText={(text) => searchFilter(text)}
             />
 
             <FlatList
                 contentContainerStyle={styles.list}
-                data={MOUNTAIN_LIST}
+                data={list}
                 keyExtractor={(item, index) => item.name}
                 ItemSeparatorComponent={() => <View style={styles.separator} />}
                 renderItem={({ item, index }) => {
